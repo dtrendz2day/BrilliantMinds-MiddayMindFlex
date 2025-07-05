36fc8c76d154689a034933f7f98d7becf74a2d01
ThePromptplug <dtrendz2day@gmail.com>
2025-07-05 17:59:14 -0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,12 @@
 
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activated.');
-});
-
-self.addEventListener('push', function(event) {
-  const options = {
-    body: "Your Midday Mind Flex suggestions are ready!",
-    icon: "icon.png",
-    badge: "icon.png"
-  };
-  event.waitUntil(
-    self.registration.showNotification("Brilliant Minds Society", options)
-  );
-});
+    self.addEventListener('push', function(event) {
+      const options = {
+        body: 'Your Midday Mind Flex quiz options are ready!',
+        icon: 'icon.png',
+        badge: 'icon.png'
+      };
+      event.waitUntil(
+        self.registration.showNotification('Brilliant Minds Society', options)
+      );
+    });
+    
