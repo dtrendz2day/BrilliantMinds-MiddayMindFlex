0c6e888795b5f29f8aa8e5523c7159adc5ebff1e
ThePromptplug <dtrendz2day@gmail.com>
2025-07-05 15:46:36 -0400

    Add files via upload
    "Add proper project structure"

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Midday Mind Flex</h1>
+      <p>Your daily brain hustle is live!</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
