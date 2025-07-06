2f1ee5fc505bc2707299ff1a4d0916420f64ea57
ThePromptplug <dtrendz2day@gmail.com>
2025-07-06 07:12:05 -0400

    Delete public/MiddayMindFlex_Final_Viral directory

--- public/MiddayMindFlex_Final_Viral/questions.js
+++
@@ -1,8 +0,0 @@
-
-const questions = [
-    {category: "Culture", question: "Which city is known as 'The Big Apple'?"},
-    {category: "Celebrities", question: "Who played the lead role in Black Panther?"},
-    {category: "History", question: "Who was the first President of the United States?"},
-    {category: "Music", question: "Who is known as the King of Pop?"},
-    {category: "Sports", question: "How many players on a basketball court per team?"}
-];
